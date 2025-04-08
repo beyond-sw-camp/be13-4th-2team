794ebbb82abb2dad769a5a2d1dded42cb8efe04e
DESKTOP-UUHNBLN\Playdata <ejrcks1690@naver.com>
2025-04-08 10:15:14 +0900

    [덕찬]

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,13 +5,16 @@ import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import router from './router'
+import { createPinia } from 'pinia'
 
 // 💡 임시 memberId 설정 (로그인 없을 때)
-if (!localStorage.getItem('memberId')) {
-    localStorage.setItem('memberId', '1'); // 교수 ID로 가정
-    localStorage.setItem('role', 'PROFESSOR'); // 필요 시 role도
-}
+// if (!localStorage.getItem('memberId')) {
+//     localStorage.setItem('memberId', '1'); // 교수 ID로 가정
+//     localStorage.setItem('role', 'PROFESSOR'); // 필요 시 role도
+// }
 
 const app = createApp(App);
+app.use(createPinia());
 app.use(router);
 app.mount('#app');
+
